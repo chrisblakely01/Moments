339ed53766c5a8b8b4baf1eda291a9cab6c5e7cb
Chris Blakely <chrisblakely@chriss-mbp.home>
2018-04-23 19:57:51 +0100

    added some ui enhancements to add journal entry

--- app/src/main/java/io/moments/activity/AddJournalEntryActivity.java
+++ app/src/main/java/io/moments/activity/AddJournalEntryActivity.java
@@ -3,6 +3,8 @@ package io.moments.activity;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 
@@ -14,17 +16,25 @@ public class AddJournalEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_journal_entry);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.add_journal_entry_toolbar);
+        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_save_journal_entry);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(
-                        view.getContext(),
-                        "Saving Journal Entry! (TODO - actually save the journal entry)",
-                        Toast.LENGTH_LONG)
-                        .show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_save_journal_entry);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(
+//                        view.getContext(),
+//                        "Saving Journal Entry! (TODO - actually save the journal entry)",
+//                        Toast.LENGTH_LONG)
+//                        .show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_add_journal_entry_toolbar, menu);
+        return true;
     }
 }
